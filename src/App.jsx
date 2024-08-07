fddca5e86695a0fbfcf520ab30b240f4d9ad2ef2
irishdelossantos <irishdeaneds@gmail.com>
2024-08-07 16:28:24 +0800

    revised

--- src/App.jsx
+++ src/App.jsx
@@ -41,12 +41,14 @@ function App() {
   };
 
   const updateTask = () => {
-    const updatedTaskList = [...taskList];
-    updatedTaskList[currentTaskIndex].task = taskName;
-    setTaskList(updatedTaskList);
-    setTaskName("");
-    setIsEditing(false);
-    setCurrentTaskIndex(null);
+    if (taskName.trim()) {
+      const updatedTaskList = [...taskList];
+      updatedTaskList[currentTaskIndex].task = taskName;
+      setTaskList(updatedTaskList);
+      setTaskName("");
+      setIsEditing(false);
+      setCurrentTaskIndex(null);
+    }
   };
 
   const toggleComplete = (index) => {
